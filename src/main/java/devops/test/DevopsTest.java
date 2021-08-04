8cc7b7b27dd075711ce053d1223f07d85c1373d0
= <->
2021-08-04 20:02:49 +0530

    Devops test

---
+++ src/main/java/devops/test/DevopsTest.java
@@ -0,0 +1,16 @@
+package devops.test;
+
+
+/*
+ * Developed By Mohit Wadhwani
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTest {
+		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			SpringApplication.run(DevopsTest.class,args);
+		}
+}
